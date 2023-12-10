76ad53758b230556df4c8d5676c7e2e2340d9897
m.g.archie <148789647+mg-archie@users.noreply.github.com>
2023-12-09 19:45:26 -0500

    refactored to have class constructors

--- library/shapes.js
+++ library/shapes.js
@@ -1,64 +1,57 @@
-// class Shape {
-//   constructor(logoText, textCol, logoShape, shapeCol) {
-//     this.logoText = logoText;
-//     this.textCol = textCol;
-//     this.logoShape = logoShape;
-//     this.shapeCol = shapeCol;
-//   }
-// }
-
-// class Triangle extends Shape {
-//   constructor(logoText, textCol, logoShape, shapeCol) {
-//     super(logoText, textCol, logoShape, shapeCol);
-//   }
-// }
-
-// class Square extends Shape {
-//   constructor(logoText, textCol, logoShape, shapeCol) {
-//     super(logoText, textCol, logoShape, shapeCol);
-//   }
-// }
-
-// class Circle extends Shape {
-//   constructor(logoText, textCol, logoShape, shapeCol) {
-//     super(logoText, textCol, logoShape, shapeCol);
-//   }
-// }
-
-// function renderText(logoText) {
-// }
-
-function renderTextCol(textCol) {
-}
-
-function renderLogoShape(answers) {
-  if (answers.logoShape === 'Square') {
+//to render squares
+class Square {
+  constructor(logoText, textCol, shapeCol) {
+    this.logoText = logoText;
+    this.textCol = textCol;
+    this.shapeCol = shapeCol;
+  }
+  render() {
     return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <rect x="50" y="20" width="150" height="150" fill='${answers.shapeCol}' />
-      <text x="125" y="125" font-size="60" text-anchor="middle" fill='${answers.textCol}'>${answers.logoText}</text>
-    </svg>`
-  } else if (answers.logoShape === 'Circle') {
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="150" cy="100" r="80" fill='${answers.shapeCol}' />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill='${answers.textCol}'>${answers.logoText}</text>
-    </svg>`
-  } else if (answers.logoShape === 'Triangle') { //need to format shape
-    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <polygon points="150, 18 244, 182 56, 182" fill='${answers.shapeCol}' />
-      <text x="150" y="125" font-size="60" text-anchor="middle" fill='${answers.textCol}'>${answers.logoText}</text>
+      <rect x="50" y="20" width="150" height="150" fill='${this.shapeCol}' />
+      <text x="125" y="125" font-size="60" text-anchor="middle" fill='${this.textCol}'>${this.logoText}</text>
     </svg>`
   }
 }
-
-// function renderShapeCol(shapeCol) {
-
-// }
-
-// function generateSvg(answers) {
-//   const renderText = renderText(answers.logoText);
-//   const renderTextCol = renderTextCol(answers.textCol);
-//   const renderLogoShape = renderLogoShape(answers.logoShape);
-//   const renderShapeCol = renderShapeCol(answers.shapeCol);
-// }
+//to render circles
+class Circle {
+  constructor(logoText, textCol, shapeCol) {
+    this.logoText = logoText;
+    this.textCol = textCol;
+    this.shapeCol = shapeCol;
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="150" cy="100" r="80" fill='${this.shapeCol}' />
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill='${this.textCol}'>${this.logoText}</text>
+    </svg>`
+  }
+}
+//to render triangles
+class Triangle {
+  constructor(logoText, textCol, shapeCol) {
+    this.logoText = logoText;
+    this.textCol = textCol;
+    this.shapeCol = shapeCol;
+  }
+  render() {
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      <polygon points="150, 18 244, 182 56, 182" fill='${this.shapeCol}' />
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill='${this.textCol}'>${this.logoText}</text>
+    </svg>`
+  }
+}
+//creates instance of new shape classes and renders with users input
+function renderLogoShape(answers) {
+  if (answers.logoShape === 'Square') {
+    const square = new Square(answers.logoText, answers.textCol, answers.shapeCol);
+    return square.render();
+  } else if (answers.logoShape === 'Circle') {
+    const circle = new Circle(answers.logoText, answers.textCol, answers.shapeCol);
+    return circle.render();
+  } else if (answers.logoShape === 'Triangle') {
+    const triangle = new Triangle(answers.logoText, answers.textCol, answers.shapeCol);
+    return triangle.render();
+  }
+}
 
 module.exports = renderLogoShape;
